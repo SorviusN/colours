96a7e8416e7ba6d27ca318a72d46c244ffb80688
SorviusN <66106310+SorviusN@users.noreply.github.com>
2020-11-01 22:33:35 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, View, TouchableWithoutFeedback, TouchableNativeFeedback } from 'react-native';
+import React, { Component, useState } from 'react';
+import { Image, ImageBackground, SafeAreaView, StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 import colours from './colours';
+
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text}> 
-      Colours App
+     Colours App
       </Text>
       <TouchableWithoutFeedback>
         <Image 
@@ -15,18 +16,18 @@ export default function App() {
         source={
           require('./assets/Red.png')}
           style={{
-            top: -20,
-            width: 100
+            top: 400,
+            right: -310,
           }}
         />
         </TouchableWithoutFeedback>
-      <TouchableWithoutFeedback onPress={()=> 
-      styles.container.backgroundColor.colours = colours.blue}>
+      <TouchableWithoutFeedback>
         <Image 
         styles={styles.container}
         source={require('./assets/Blue.png')}
-          style={{
-          top: -10,
+        style={{
+          right: -160,
+          top: 350
         }}
         /> 
       </TouchableWithoutFeedback>
@@ -34,21 +35,35 @@ export default function App() {
         <Image 
         styles={styles.container}
         source={require('./assets/Green.png')}
+        style={{
+          top: 300,
+          right: -10
+          
+        }}
         />
         </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colours.green,
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  text: {
-    flex: 1,
-    fontSize: 30,
-  },
-});
+
+const styles = () => {
+  const [background, setBackground] = useState("#000");
+  
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: {background},
+      alignItems: 'baseline',
+      justifyContent: 'flex-start',
+      flexDirection: 'column',
+    },
+    text: {
+      flex: 1,
+      fontSize: 30,
+      alignItems: 'flex-start',
+      flexDirection: 'row',
+      justifyContent: 'flex-start'
+    },
+  });
+}
+
